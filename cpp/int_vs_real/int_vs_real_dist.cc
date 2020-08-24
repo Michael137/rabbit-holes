0e87412cf86d6570a1d2d8d480967b0fbb004433
Michael137 <michaelbuch12@gmail.com>
2020-08-24 12:11:09 -0400

    plots, fix csv data

--- cpp/int_vs_real/int_vs_real_dist.cc
+++ cpp/int_vs_real/int_vs_real_dist.cc
@@ -1,7 +1,7 @@
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <random>
-#include <fstream>
 
 std::mt19937 random_engine{std::random_device()()};
 
@@ -11,9 +11,8 @@ std::vector<T> generate_random( Distribution distribution, int num_elements )
 {
 	std::vector<T> data( num_elements );
 
-	std::generate_n( data.begin(), num_elements, [&]() {
-		return distribution( random_engine );
-	} );
+	std::generate_n( data.begin(), num_elements,
+	                 [&]() { return distribution( random_engine ); } );
 	return data;
 }
 
@@ -21,11 +20,14 @@ template<typename T> std::vector<T> create_data( int num_elements )
 {
 	if constexpr( std::is_same_v<T, float> )
 		return generate_random<float>(
-		    std::uniform_real_distribution<float>( -127.0f, 127.0f ),
+		    std::uniform_real_distribution<float>( -128.0f, 128.0f ),
 		    num_elements );
+	if constexpr( std::is_same_v<T, int32_t> )
+		return generate_random<int32_t>(
+		    std::uniform_int_distribution<int32_t>( -128, 128 ), num_elements );
 	if constexpr( std::is_same_v<T, int8_t> )
 		return generate_random<int8_t>(
-		    std::uniform_int_distribution<int32_t>( -127, 127 ), num_elements );
+		    std::uniform_int_distribution<int32_t>( -128, 128 ), num_elements );
 }
 
 int main()
@@ -37,8 +39,8 @@ int main()
 	          << ( std::chrono::system_clock::now() - start ).count() << '\n';
 	{
 		std::ofstream fs{"float.csv", std::ios::out};
-		for(auto&& e : float_data)
-			fs << e << ',' << '\n';
+		for( auto&& e: float_data )
+			fs << e << '\n';
 		fs.close();
 	}
 
@@ -48,8 +50,19 @@ int main()
 	          << ( std::chrono::system_clock::now() - start ).count() << '\n';
 	{
 		std::ofstream fs{"int8.csv", std::ios::out};
-		for(auto&& e : int8_data)
-			fs << e << ',' << '\n';
+		for( auto&& e: int8_data )
+			fs << (int)e << '\n';
+		fs.close();
+	}
+
+	start           = std::chrono::system_clock::now();
+	auto int32_data = create_data<int32_t>( num_elements );
+	std::cout << "Time (int32_t): "
+	          << ( std::chrono::system_clock::now() - start ).count() << '\n';
+	{
+		std::ofstream fs{"int32.csv", std::ios::out};
+		for( auto&& e: int32_data )
+			fs << e << '\n';
 		fs.close();
 	}
 
